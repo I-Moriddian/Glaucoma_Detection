5297e8ecb420df714d93897305937a49d8ef0620
I-Moriddian <142854102+I-Moriddian@users.noreply.github.com>
2023-09-26 08:13:19 -0400

    Update train.py

--- train.py
+++ train.py
@@ -6,12 +6,12 @@ from keras.layers.normalization import BatchNormalization
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
 model = Sequential()
-
+#creates the neural network layers and decides the size of the image that it will take.
 model.add(Conv2D(64, (9, 9), input_shape=(100,100,3),padding='same'))
 model.add(BatchNormalization())
 model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
-
+#creates more layers
 model.add(Conv2D(128, (5, 5), padding='same'))
 model.add(BatchNormalization())
 model.add(Activation('relu'))
@@ -48,9 +48,10 @@ model.add(Dropout(0.5))
 model.add(Dense(1))
 model.add(BatchNormalization())
 model.add(Activation('sigmoid'))
-
+#uses binary/it either has glaucoma or it doesn't
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 
+#creates an image crop for the photo
 train_datagen = ImageDataGenerator(rescale = 1./255,
                                    rotation_range=30,
                                    shear_range=0.2,
@@ -59,23 +60,25 @@ train_datagen = ImageDataGenerator(rescale = 1./255,
                                     fill_mode='nearest')
 
 test_datagen = ImageDataGenerator(rescale = 1./255)
-
+#sets the size of the data/number of photos used for training
 training_set = train_datagen.flow_from_directory('/content/drive/My Drive/data/train',
                                                  target_size = (100, 100),
-                                                 batch_size = 64,
+                                                 batch_size = 200,
                                                  class_mode = 'binary')
-
+#sets the number of photos used for testing
 test_set = test_datagen.flow_from_directory('/content/drive/My Drive/data/val',
                                             target_size = (100, 100),
-                                            batch_size = 64,
+                                            batch_size = 50,
                                             class_mode = 'binary')
 my_callbacks = [
    
     tf.keras.callbacks.ReduceLROnPlateau(factor=0.1, patience=3, min_lr=0.00001, verbose=1),
-    tf.keras.callbacks.ModelCheckpoint('my_model2.h5', 
+    #invalid code below, will change later
+    #tf.keras.callbacks.ModelCheckpoint('my_model2.h5', 
     verbose=1, save_best_only=True, save_weights_only=False) 
     ]
 
 model.fit(training_set, epochs=200, validation_data = test_set, callbacks=my_callbacks)
 
-model.save('my_model2.h5')
+#invalid code below, will change later
+#model.save('my_model2.h5')
